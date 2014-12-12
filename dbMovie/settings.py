2eb59f8a3c0036910517275ca22d846f2150cb06
silenceper <silenceper@gmail.com>
2014-12-12 16:59:26 +0800

    init project

---
+++ dbMovie/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for dbMovie project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'dbMovie'
+
+SPIDER_MODULES = ['dbMovie.spiders']
+NEWSPIDER_MODULE = 'dbMovie.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'dbMovie (+http://www.yourdomain.com)'
